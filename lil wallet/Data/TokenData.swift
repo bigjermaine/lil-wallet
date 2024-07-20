1c2ccb5d24bc792b193e720b6df671995fbc2240
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-07-20 10:38:53 +0100

    Reduced the Data file into smaller bits of files

---
+++ lil wallet/Data/TokenData.swift
@@ -0,0 +1,8 @@
+//
+//  TokenData.swift
+//  lil wallet
+//
+//  Created by MacBook AIR on 20/07/2024.
+//
+
+import Foundation
---
+++ lil wallet/Data/TransactionData.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionData.swift
+//  lil wallet
+//
+//  Created by MacBook AIR on 20/07/2024.
+//
+
+import Foundation
